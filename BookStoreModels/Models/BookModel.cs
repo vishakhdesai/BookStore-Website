130f3c1d816e1337ffdfdc688a36c6c70269d33b
Vishakh <vishakhdesai@gmail.com>
2022-06-06 14:57:14 +0530

    first commit

---
+++ BookStoreModels/Models/BookModel.cs
@@ -0,0 +1,34 @@
+﻿using BookStoreModels.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreModels.Models
+{
+    public class BookModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public string? Base64image { get; set; }
+        public int Categoryid { get; set; }
+        public int? Publisherid { get; set; }
+        public int? Quantity { get; set; }
+
+        public BookModel() { }
+        public BookModel(Book book)
+        {
+            this.Id = book.Id;
+            this.Name = book.Name;
+            this.Price = book.Price;
+            this.Description = book.Description;
+            this.Base64image = book.Base64image;
+            this.Categoryid = book.Categoryid;
+            this.Publisherid = book.Publisherid;
+            this.Quantity = book.Quantity;
+        }
+    }
+}
---
+++ BookStoreModels/Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreModels.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; } = null!;
+        public string Lastname { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public int Roleid { get; set; }
+    }
+}
